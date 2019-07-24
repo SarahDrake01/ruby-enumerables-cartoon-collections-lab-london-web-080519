45cce36288642bdf69b3383c0e5e7d4ce20f0791
Sarah Drake <jussaradrake@outlook.com>
2019-07-24 16:38:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,17 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map{|element|element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  counter = 0
+  while counter < short_words.length do 
+    if (short_words[counter].length > 4)
+      return true
+    end
+    counter +=1
+  end
+  return false
+end
+  
 end
 
 def find_the_cheese# code an argument here
